32d9df36a706465e368c3c06fb809c0bed65a302
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-04-11 14:28:36 -0500

    add footer

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -13,8 +13,10 @@ import {
   MenuItem,
   MenuList,
   useToast,
+  Text,
 } from "@chakra-ui/react";
 import { FiUser, FiSettings, FiLogOut } from "react-icons/fi";
+import { TbCode } from "react-icons/tb";
 
 import { endSession } from "@/utils";
 import { loadAuthStateFromLocalStorage } from "@/utils/storage";
@@ -49,45 +51,57 @@ function Layout({ children }: { children: ReactNode }) {
   };
   return (
     <>
-      <Box w="100%" h="100px" p="1rem" bgColor="#f2f2f2">
-        <Flex align="center" justify="space-between" w="100%" h="100%">
-          <Image
-            src={logo.src}
-            alt="Nanni Clock"
-            height={80}
-            width={80}
-            // style={{ borderRadius: "8px" }}
-          />
+      <Flex flexDirection="column"></Flex>
+      <Box w="100%" h="100%" flex="1">
+        <Box w="100%" h="100px" p="1rem" bgColor="#f2f2f2">
+          <Flex align="center" justify="space-between" w="100%" h="100%">
+            <Image
+              src={logo.src}
+              alt="Nanni Clock"
+              height={80}
+              width={80}
+              // style={{ borderRadius: "8px" }}
+            />
 
-          <Heading size="xs" textAlign="center" p="0 1.5rem">
-            Clock in your Nanny's work and generate invoices easily
-          </Heading>
+            <Heading size="xs" textAlign="center" p="0 1.5rem">
+              Clock in your Nanny's work and generate invoices easily
+            </Heading>
 
-          <Menu>
-            <MenuButton
-              ml="0.5rem"
-              as={IconButton}
-              variant="ghost"
-              icon={<FiUser />}
-            ></MenuButton>
-            <MenuList>
-              <MenuItem icon={<FiUser />}>
-                <span suppressHydrationWarning>{getDisplayName()}</span>
-              </MenuItem>
-              <MenuItem
-                icon={<FiSettings />}
-                onClick={() => router.push("/home/settings")}
-              >
-                Settings
-              </MenuItem>
-              <MenuItem icon={<FiLogOut />} onClick={endSession}>
-                Logout
-              </MenuItem>
-            </MenuList>
-          </Menu>
+            <Menu>
+              <MenuButton
+                ml="0.5rem"
+                as={IconButton}
+                variant="ghost"
+                icon={<FiUser />}
+              ></MenuButton>
+              <MenuList>
+                <MenuItem icon={<FiUser />}>
+                  <span suppressHydrationWarning>{getDisplayName()}</span>
+                </MenuItem>
+                <MenuItem
+                  icon={<FiSettings />}
+                  onClick={() => router.push("/home/settings")}
+                >
+                  Settings
+                </MenuItem>
+                <MenuItem icon={<FiLogOut />} onClick={endSession}>
+                  Logout
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Box>
+        <Box p="1rem">{children}</Box>
+      </Box>
+      <br />
+      <Box p="1rem" w="100%" backgroundColor="#f9f9f9">
+        <Flex align="center" justify="center">
+          <Text fontSize="small">Made by Satshree Shrestha</Text>
+          <Text fontSize="larger" ml="0.5rem">
+            <TbCode />
+          </Text>
         </Flex>
       </Box>
-      <Box p="1rem">{children}</Box>
     </>
   );
 }
