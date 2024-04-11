4bf53bd2ce41438573978098d4dfb39f5aae7d6e
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-04-11 13:04:09 -0500

    comment out redux provider

--- src/app/storeProvider.tsx
+++ src/app/storeProvider.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import React from "react";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 
-import store from "@/redux/store";
+// import store from "@/redux/store";
 
 function StoreProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <>
+      {/* <Provider store={store}> */}
+      {children}
+      {/* </Provider>; */}
+    </>
+  );
 }
 
 export default StoreProvider;
